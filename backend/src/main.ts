96250cd30177b795762493dfe57e4974973933b2
HilquiasAbias <hilquias.dev@gmail.com>
2023-06-19 13:16:59 -0300

    adcionando cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*'
+  })
   
   const config = new DocumentBuilder()
     .setTitle('Laica: controle de acesso')
